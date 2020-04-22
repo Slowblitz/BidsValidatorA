4e11e965b690972c41a249f5fb9d8fdb4b032e78
Slowblitz <jeremygarcia937@gmail.com>
2020-04-22 16:14:24 +0200

    Update test_isbids.py

--- resources/Tests/test_isbids.py
+++ resources/Tests/test_isbids.py
@@ -14,7 +14,7 @@ class test_bids(unittest.TestCase):
 
     def test_bids_dataset_1(self):
         names=list()
-        test1=PATH+"/Tests/DataSets/ds001/Data"
+        test1="Tests/DataSets/ds001/Data"
         dic_data = json.loads(
         json.dumps(BVA.path_hierarchy(test1), indent=2, sort_keys=True))
         names = BVA.get_name_in_dir([dic_data], names)
@@ -22,7 +22,7 @@ class test_bids(unittest.TestCase):
 
     def test_bids_dataset_2(self):
         names=list()
-        test1=PATH+"/Tests/DataSets/ds002/data"
+        test1="Tests/DataSets/ds002/data"
         dic_data = json.loads(
         json.dumps(BVA.path_hierarchy(test1), indent=2, sort_keys=True))
         names = BVA.get_name_in_dir([dic_data], names)
@@ -30,23 +30,23 @@ class test_bids(unittest.TestCase):
 
     def test_bids_dataset_3(self):
         names=list()
-        test1=PATH+"/Tests/DataSets/ds003/data"
+        test1="Tests/DataSets/ds003/data"
         dic_data = json.loads(
         json.dumps(BVA.path_hierarchy(test1), indent=2, sort_keys=True))
         names = BVA.get_name_in_dir([dic_data], names)
         self.assertEqual(BVA.is_bids(names), False)
 
-    def test_bids_dataset_3(self):
+    def test_bids_dataset_4(self):
         names=list()
-        test1=PATH+"/Tests/DataSets/ds004/data"
+        test1="Tests/DataSets/ds004/data"
         dic_data = json.loads(
         json.dumps(BVA.path_hierarchy(test1), indent=2, sort_keys=True))
         names = BVA.get_name_in_dir([dic_data], names)
         self.assertEqual(BVA.is_bids(names), False)
 
-    def test_bids_dataset_3(self):
+    def test_bids_dataset_5(self):
         names=list()
-        test1=PATH+"/Tests/DataSets/ds005/data"
+        test1="Tests/DataSets/ds005/data"
         dic_data = json.loads(
         json.dumps(BVA.path_hierarchy(test1), indent=2, sort_keys=True))
         names = BVA.get_name_in_dir([dic_data], names)
